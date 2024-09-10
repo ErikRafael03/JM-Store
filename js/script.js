5adfb8e9e5d50214ef1d70c930f0e62d16780ce0
Erik Rafael Alves Santos <erikrafaelasantos@gmail.com>
2024-09-10 20:33:16 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// MENU MOBILE 
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 
@@ -7,3 +8,20 @@ btnMenu.addEventListener('click', ()=>{
 menu.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+// MENU MOBILE 
+
+// BTN TAMANHO
+const sizeButtons = document.querySelectorAll('.size-btn');
+
+    sizeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Remove a classe "selected" de todos os botões
+            sizeButtons.forEach(btn => btn.classList.remove('selected'));
+            // Adiciona a classe "selected" ao botão clicado
+            button.classList.add('selected');
+        });
+    });
+
+function changeImage(imageSrc) {
+    document.getElementById('mainDisplay').src = imageSrc;
+}
